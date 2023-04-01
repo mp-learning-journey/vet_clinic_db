83755026323e07efb58b4645b0d1fbf34ad1143e
mrprotocoll <lekanvgbg@gmail.com>
2023-04-01 02:26:09 +0100

    Added query to List the name of all animals that are neutered and have less than 3 escape attempts.

--- queries.sql
+++ queries.sql
@@ -2,3 +2,13 @@
 
 -- Find all animals whose name ends in "mon".
 SELECT * FROM animals WHERE name LIKE '%mon';
+
+-- List the name of all animals born between 2016 and 2019
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+-- List the name of all animals that are neutered and have less than 3 escape attempts.
+-- List the date of birth of all animals named either "Agumon" or "Pikachu".
+-- List name and escape attempts of animals that weigh more than 10.5kg
+-- Find all animals that are neutered.
+-- Find all animals not named Gabumon.
+-- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
