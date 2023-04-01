20970603c332da1ed4fd347ba53bba6ce93adea4
mrprotocoll <lekanvgbg@gmail.com>
2023-04-01 01:54:38 +0100

    Added schema to create animal database and table. inserted records to the animal table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts INT DEFAULT 0,
+    neutered BOOLEAN,
+    weight_kg DECIMAL DEFAULT 0
 );
