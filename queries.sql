0454b255d67ab643d6ca3a3a0f6c3133b158f09d
mrprotocoll <lekanvgbg@gmail.com>
2023-04-01 10:00:43 +0100

    Added transaction to delete and use savepoint, rollover and commit to update animals weight

--- queries.sql
+++ queries.sql
@@ -22,20 +22,39 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg).
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+-- start transaction
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
 
+-- start transaction
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
 SELECT * FROM animals;
 
+-- start transaction
 BEGIN;
 DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
+
+-- start transaction
+BEGIN;
+-- Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+-- create savepoint
+SAVEPOINT SP1;
+-- update all animals' weight to be their weight multiplied by -1
+UPDATE animals SET weight_kg = (weight_kg * -1);
+-- Rollback to the savepoint
+ROLLBACK TO SP1;
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0;
+-- commit transaction
+COMMIT;
 SELECT * FROM animals;
