f489169473d4335ca2e56cb9397ac30a058b4fb4
mrprotocoll <lekanvgbg@gmail.com>
2023-04-01 13:52:04 +0100

    Added record into species table

--- data.sql
+++ data.sql
@@ -31,3 +31,7 @@ INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
 INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
 INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+-- insert data into the species table
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
