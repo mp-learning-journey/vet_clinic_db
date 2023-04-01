463b095aefd72f1be007beaab282beebad3f975e
mrprotocoll <lekanvgbg@gmail.com>
2023-04-01 02:27:18 +0100

    Added query to List the date of birth of all animals named either Agumon or Pikachu

--- queries.sql
+++ queries.sql
@@ -7,7 +7,11 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
+
 -- List the date of birth of all animals named either "Agumon" or "Pikachu".
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
 -- List name and escape attempts of animals that weigh more than 10.5kg
 -- Find all animals that are neutered.
 -- Find all animals not named Gabumon.
