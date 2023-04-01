32c34bbd3d5c3fe5c478896976e87f3b681e6f8d
mrprotocoll <lekanvgbg@gmail.com>
2023-04-01 08:45:04 +0100

    added a transaction to update species and commit

--- queries.sql
+++ queries.sql
@@ -1,9 +1,7 @@
-/*Queries that provide answers to the questions from all projects.*/
-
 -- Find all animals whose name ends in "mon".
 SELECT * FROM animals WHERE name LIKE '%mon';
 
--- List the name of all animals born between 2016 and 2019
+-- List the name of all animals born between 2016 and 2019.
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
@@ -12,7 +10,7 @@ SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
 -- List the date of birth of all animals named either "Agumon" or "Pikachu".
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 
--- List name and escape attempts of animals that weigh more than 10.5kg
+-- List name and escape attempts of animals that weigh more than 10.5kg.
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 -- Find all animals that are neutered.
@@ -21,11 +19,17 @@ SELECT * FROM animals WHERE neutered = true;
 -- Find all animals not named Gabumon.
 SELECT * FROM animals WHERE name != 'Gabumon';
 
--- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+-- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg).
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
 SELECT * FROM animals;
