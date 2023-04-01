765eeff9fee38fb3628ddde63859621d39053968
mrprotocoll <lekanvgbg@gmail.com>
2023-04-01 11:07:47 +0100

    Added basic queries to perform basic aggregate operations

--- queries.sql
+++ queries.sql
@@ -22,6 +22,7 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg).
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.
 -- start transaction
 BEGIN;
 UPDATE animals SET species = 'unspecified';
@@ -58,3 +59,5 @@ UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0;
 -- commit transaction
 COMMIT;
 SELECT * FROM animals;
+
+-- How many animals are there?
