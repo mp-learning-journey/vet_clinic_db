efb286aa67fd9fe88198bf35854f9cc53ac54827
mrprotocoll <lekanvgbg@gmail.com>
2023-04-01 02:38:37 +0100

    Added query to Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)

--- queries.sql
+++ queries.sql
@@ -22,3 +22,4 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
